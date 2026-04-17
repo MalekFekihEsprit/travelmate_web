30ea4a5f8c6f1988fa03081a88e450c7c7331d4f
FekihMalek <malekfekih01@gmail.com>
2026-04-17 20:55:20 +0100

    added login and sign up with fb,microsoft,google,github (-facebook and microsoft require https not http)

--- importmap.php
+++ importmap.php
@@ -1,20 +1,8 @@
 <?php
 
-/**
- * Returns the importmap for this application.
- *
- * - "path" is a path inside the asset mapper system. Use the
- *     "debug:asset-map" command to see the full list of paths.
- *
- * - "entrypoint" (JavaScript only) set to true for any module that will
- *     be used as an "entrypoint" (and passed to the importmap() Twig function).
- *
- * The "importmap:require" command can be used to add new entries to this file.
- */
 return [
     'app' => [
         'path' => './assets/app.js',
-        'chart.js' => 'node_modules/chart.js/dist/chart.js', 
         'entrypoint' => true,
     ],
     '@hotwired/stimulus' => [
@@ -27,7 +15,7 @@ return [
         'version' => '7.3.0',
     ],
     'chart.js' => [
-        'version' => '4.5.1',
+        'url' => 'https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.js',
     ],
     '@kurkle/color' => [
         'version' => '0.3.4',
