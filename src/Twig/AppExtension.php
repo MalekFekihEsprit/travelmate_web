7f4cc228ce2c60512338dada9d243d84de53cb32
meriembgl <meriem.bouguila@esprit.tn>
2026-05-09 13:22:43 +0100

    correction navigation

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,17 +2,33 @@
 
 namespace App\Twig;
 
+use App\Repository\CategorieRepository;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    public function __construct(private CategorieRepository $categorieRepository) {}
+
     public function getFilters(): array
     {
         return [
             new TwigFilter('sum', [$this, 'arraySum']),
             new TwigFilter('max', [$this, 'getMax']),
         ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('nav_categories_with_activites', [$this, 'getNavCategories']),
+        ];
+    }
+
+    public function getNavCategories(): array
+    {
+        return $this->categorieRepository->findAll();
     }
 
     public function getMax(array $array): mixed
